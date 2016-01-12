4e5d032bf40b6acbb55588fe29b1f17f11abfe41
Andy <andy@errorstudio.co.uk>
2016-01-12 12:34:20 +0000

    tidying of files and contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,11 @@
 	$(document).ready(function() {
 
 
+		// General ----------
+
 
 		// Clean up empty <p>s by removing whitespace, so :empty can then target and hide them.
+
 		if ($('.SpektrixPage p').exists) {
 			$('.SpektrixPage p').each(function(){
 				if ($(this).text().trim().length < 1 && $(this).children().length < 1) {
@@ -15,7 +18,9 @@
 			});
 		}
 
+
 		// …and WikiText holders while we're at it. NOT QUITE WORKING
+
 		if ($('.SpektrixPage div[class$="WikiText"], .SpektrixPage div[class$="WikiText"] > div')) {
 			$('.SpektrixPage div[class$="WikiText"], .SpektrixPage div[class$="WikiText"] > div').each(function(){
 				if ($(this).text().trim().length < 1 && $(this).children().length < 1) {
@@ -25,12 +30,10 @@
 		}
 
 
+		// Specific pages ----------
 
 
-		// See what page we're on
-		if ($('.SpektrixPage.EventsList').exists()) {
-
-		}
+		// EventDetails
 
 		if ($('.SpektrixPage.EventDetails').exists()) {
 			
@@ -39,12 +42,18 @@
 
 		}
 
+
+		// ChooseSeats
+
 		if ($('.SpektrixPage.ChooseSeats').exists()) {
 
 			// Fix inconsistent capitalisation in heading
 			$('.ChooseSeatsHeading h1').replaceWith('<h1>Choose seats</h1>');
 
 		}
+
+
+		// EditTickets
 
 		if ($('.SpektrixPage.EditTickets').exists()) {
 			
@@ -54,6 +63,9 @@
 			$('h1.EditTicketsHeading').append('<span class="booking-for">Booking tickets for: <b>' + eventName + '</b></span>');
 
 		}
+
+
+		// Basket2
 
 		if ($('.SpektrixPage.Basket2').exists()) {
 			
@@ -71,6 +83,9 @@
 			$('.SpektrixPage.Basket2 .OptionalMessage').remove();
 		}
 
+
+		// EventsList
+
 		if ($('.SpektrixPage.EventsList').exists()) {
 			
 			// The heading doesn't describe the page and is inconsistent, let's replace it
@@ -79,11 +94,14 @@
 		}
 
 
+		// After
+
 		// Stick an event name in the header if we can find one
 		if ($('.EventName').exists()) {
 			var eventName = $('.EventName').html();
 			$('h1').append('<span class="booking-for">Booking tickets for: <b>' + eventName + '</b></span>');
 		}
+
 
 
 	});
